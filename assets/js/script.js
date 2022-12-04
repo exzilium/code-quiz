e883ba62e9b23234459451bbd0511add30dad818
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-04 13:01:41 -0800

    Create logic to compare answer selected by user and correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var startClass = document.querySelector("#start-section").className;
 // variables for Question and Answer elements
 var questionSection = document.querySelector("#q-section");
 var questionHead = document.querySelector("#q-head");
+var questionBtnDiv = document.querySelector("#q-btns");
 var answerBtn1 = document.querySelector("#btn1");
 var answerBtn2 = document.querySelector("#btn2");
 var answerBtn3 = document.querySelector("#btn3");
@@ -17,10 +18,13 @@ var answerBtn4 = document.querySelector("#btn4");
 
 var question1 = {
   question: "Question one text",
-  correct: "Correct Answer",
-  incorrect1: "Incorrect Answer 1",
-  incorrect2: "Incorrect Answer 2",
-  incorrect3: "Incorrect Answer 3",
+  answer1: "Incorrect Answer 1",
+  answer2: "Incorrect Answer 2",
+  answer3: "Incorrect Answer 3",
+  answer4: "Correct Answer",
+  correct: function () {
+    return this.answer4;
+  },
 };
 
 // ^ End of global variable ^
@@ -41,11 +45,17 @@ var question1Text = function () {
   // Populate question header with question1 object question property
   questionHead.textContent = question1.question;
 
-  // Populate answer buttons
-  answerBtn1.textContent = question1.correct;
-  answerBtn2.textContent = question1.incorrect1;
-  answerBtn3.textContent = question1.incorrect2;
-  answerBtn4.textContent = question1.incorrect3;
+  // Populate answer button text
+  answerBtn1.textContent = question1.answer1;
+  answerBtn2.textContent = question1.answer2;
+  answerBtn3.textContent = question1.answer3;
+  answerBtn4.textContent = question1.answer4;
+
+  // Update answer button data-correct attribute to compare click event with correct answer
+  answerBtn1.dataset.correct = question1.correct();
+  answerBtn2.dataset.correct = question1.correct();
+  answerBtn3.dataset.correct = question1.correct();
+  answerBtn4.dataset.correct = question1.correct();
 };
 
 // ^ End of Functions ^
@@ -61,4 +71,22 @@ startBtn.addEventListener("click", function () {
   questionSectionDisplay();
   // Populate first question and answer text
   question1Text();
+});
+
+// Answer button click event listener
+questionBtnDiv.addEventListener("click", function (event) {
+  console.log(
+    "answer button clicked:  " +
+      event.target.id +
+      ".  Correct: " +
+      question1.correct()
+  );
+  // if correct run populate next question
+  if (event.target.textContent === question1.correct()) {
+    console.log("Correct Answer selected!");
+  } else {
+    console.log("Wrong answer, bucko!");
+  }
+
+  // if incorrect, append "wrong" to section
 });
