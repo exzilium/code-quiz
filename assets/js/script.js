7df13c3e424af5ebcc3222d378b4d9bc01f5ad6b
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-04 11:08:45 -0800

    Update JS start button click to change class of start to hidden and question to visible to display question section upon start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,23 @@
-var startContainer = document.querySelector("#start-container");
-var startHeader = document.querySelector("#start-head");
-var startDesc = document.querySelector("#start-desc");
+// variables for START elements
+var startSection = document.querySelector("#start-section");
 var startBtn = document.querySelector("#start-btn");
+var startClass = document.querySelector("#start-section").className;
 
-// To be called when user clicks start button
-startElementRemove = function () {
-  startHeader.remove();
-  startDesc.remove();
-  startBtn.remove();
+// varibles for QUESTION elements
+var questionSection = document.querySelector("#q-section");
+
+// Functions to be called when user clicks start button
+startSectionHide = function () {
+  startSection.className = "start hidden";
+};
+
+questionSectionDisplay = function () {
+  questionSection.className = "question visible";
 };
 
 startBtn.addEventListener("click", function () {
   // hide starting elements
   console.log("start!");
-  startElementRemove();
-
-  // create game elements
-  var questionEl = document.createElement("h2");
-  var answerList = document.createElement("ul")
-  var answerEl1 = document.createElement("li");
-  var answerEl2 = document.createElement("li");
-  var answerEl3 = document.createElement("li");
-  var answerEl4 = document.createElement("li");
-
-  // add text to game elements
-  questionEl.textContent = "QUESTION HEADER";
-  answerEl1.textContent = "answer 1";
-  answerEl2.textContent = "answer 2";
-  answerEl3.textContent = "answer 3";
-  answerEl4.textContent = "answer 4";
-
-  // append game elements
-  startContainer.append(questionEl);
-  startContainer.append(answerList)
-  answerList.append(answerEl1);
-  answerList.append(answerEl2);
-  answerList.append(answerEl3);
-  answerList.append(answerEl4);
+  startSectionHide();
+  questionSectionDisplay();
 });
